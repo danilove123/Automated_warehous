618949b9ffd29f6619a951e0bf84950e4ef1bc1b
danilove <danilov2017daniil@mail.ru>
2024-02-03 23:11:47 +1000

    last changes

--- test_task/task.py
+++ test_task/task.py
@@ -1,6 +1,6 @@
 import argparse
-
-from sqlalchemy import func
+import csv
+from sqlalchemy import func, and_, or_, case
 
 from test_task.databases import sync_sessionmaker_1, sync_sessionmaker_2
 from test_task.models import User, Post, Logs, EventType, SpaceType
@@ -13,46 +13,85 @@ from test_task.models import User, Post, Logs, EventType, SpaceType
 
 user_login = "daniil228339"
 
-with sync_sessionmaker_2() as session_1:
-    # logs = session_1.query(Logs).filter(Logs.space_type_id.name == "global")
 
-    # Возвращает список состоящий из кортежей (пост,кол-во комментов)
-    # Пример: logs=[(0,5)] => Данный пользователь оставил 5 коментов только к одному посту c post_id = 0
-    logs = session_1.query(Logs.post_id, func.count(Logs.id)). \
-        join(EventType). \
-        join(SpaceType). \
-        filter(EventType.name == "comment", SpaceType.name == "post", Logs.user_id == 2). \
-        group_by(Logs.post_id). \
-        all()
+def generate_statistics_comments():
+    filename = "comments.csv"
+    data = [
+        ['user_login', 'post_header', 'author_login', 'comment_count']
+    ]
 
-    for tupl in logs:
-        post_id = tupl[0]
+    with sync_sessionmaker_1() as session_1:
+        user = session_1.query(User).filter(User.login == user_login).first()
 
-with sync_sessionmaker_1() as session_1:
-    user = session_1.query(User).filter(User.login == user_login).first()
-    # user_posts = session.query(Post).filter(Post.author_id == user.id).all()
+        with sync_sessionmaker_2() as session_2:
+            # Возвращает список состоящий из кортежей (пост,кол-во комментов)
+            # Пример: logs=[(0,5)] => Данный пользователь оставил 5 коментов к посту c post_id = 0
+            logs = session_2.query(Logs.post_id, func.count(Logs.id)). \
+                join(EventType). \
+                join(SpaceType). \
+                filter(EventType.name == "comment", SpaceType.name == "post", Logs.user_id == user.id). \
+                group_by(Logs.post_id). \
+                all()
 
-    with sync_sessionmaker_2() as session_2:
-        # logs = session_1.query(Logs).filter(Logs.space_type_id.name == "global")
+            for tupl in logs:
+                post_id = tupl[0]
+                count_comments = tupl[1]
+                post = session_1.query(Post).filter(Post.id == post_id).first()
+                author = session_1.query(User).join(Post).filter(User.author_id == post.author_id).first()
+                data.append([user_login, post.header, author.login, count_comments])
 
-        # Возвращает список состоящий из кортежей (пост,кол-во комментов)
-        # Пример: logs=[(0,5)] => Данный пользователь оставил 5 коментов только к одному посту c post_id = 0
-        logs = session_2.query(Logs.post_id, func.count(Logs.id)). \
-            join(EventType). \
-            join(SpaceType). \
-            filter(EventType.name == "comment", SpaceType.name == "post", Logs.user_id == user.id). \
-            group_by(Logs.post_id). \
-            all()
+                # print(f"Логин пользователя: {user_login}\n"
+                #       f"Заголовок: {post.header}\n"
+                #       f"Логин автора: {author.login}\n"
+                #       f"Кол-во коментов: {count_comments}")
 
-        print(logs)
-        for tupl in logs:
-            post_id = tupl[0]
-            count_comments = tupl[1]
-            post = session_1.query(Post).filter(Post.id == post_id).first()
-            author = session_1.query(User).join(Post).filter(User.author_id == post.author_id).first()
-            print(f"Логин пользователя: {user_login}\n"
-                  f"Заголовок: {post.header}\n"
-                  f"Логин автора: {author.login}\n"
-                  f"Кол-во коментов: {count_comments}")
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
 
+def generate_statistics_events():
+    with sync_sessionmaker_1() as session_1:
+        user = session_1.query(User).filter(User.login == user_login).first()
+
+        with sync_sessionmaker_2() as session_2:
+
+            date_list = session_2.query(Logs.datetime).filter(Logs.user_id == 1).order_by(Logs.datetime.asc()).all()
+
+            logs = session_2.query(Logs.datetime, func.count(Logs.id)). \
+                join(EventType). \
+                join(SpaceType). \
+                filter(EventType.name == "login", SpaceType.name == "global", Logs.user_id == 1). \
+                group_by(Logs.datetime). \
+                all()
+
+            logs_1 = session_2.query(Logs.datetime, func.count(Logs.id)). \
+                join(EventType). \
+                join(SpaceType). \
+                filter(EventType.name == "logout", SpaceType.name == "global", Logs.user_id == 1). \
+                group_by(Logs.datetime). \
+                all()
+
+            logs_2 = session_2.query(Logs.datetime, func.count(Logs.id)). \
+                join(EventType). \
+                join(SpaceType). \
+                filter(SpaceType.name == "blog", Logs.user_id == 1). \
+                group_by(Logs.datetime). \
+                all()
+
+
+
+            #
+            # print(logs_1)
+
+            # for tupl in logs:
+            #     date = tupl[0]
+            #     count_comments = tupl[1]
+            #     post = session_1.query(Post).filter(Post.id == post_id).first()
+            #     author = session_1.query(User).join(Post).filter(User.author_id == post.author_id).first()
+            #     data.append([user_login,post.header,author.login,count_comments])
+
+
+
+
+generate_statistics_events()
